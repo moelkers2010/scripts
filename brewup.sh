31c9ed4cdb4d225ea950b1868dd992097e1e6906
Michael Oelkers <moelkers2010@gmail.com>
2015-07-20 11:21:17 -0500

    Added date and formatting to brewup.sh

--- brewup.sh
+++ brewup.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
+echo"------------------------------------------------"-
+date 
+echo""
+echo""
 brew update >> ~/Desktop/brewlog.txt
 brew upgrade --all >> ~/Desktop/brewlog.txt
+echo""
+echo"------------------------------------------------"
