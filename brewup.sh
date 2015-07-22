1d32570ef9211e6d5dd2c2550e93247ab5c2889f
Michael Oelkers <moelkers2010@gmail.com>
2015-07-22 14:24:21 -0500

    Fixing fubared redirects on formatting and dating lines.

--- brewup.sh
+++ brewup.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 echo "------------------------------------------------"
-date >> ~/Desktop >> brewlog.txt 
-echo"" >> ~/Desktop >> brewlog.txt
-echo"" >> ~/Desktop >> brewlog.txt
-brew update >> ~/Desktop >> brewlog.txt
-brew upgrade --all >> ~/Desktop >> brewlog.txt
-echo"" >> ~/Desktop >> brewlog.txt
-echo "------------------------------------------------" >> ~/Desktop >> brewlog.txt
+date >> ~/Desktop/brewlog.txt 
+echo"" >> ~/Desktop/brewlog.txt
+echo"" >> ~/Desktop/brewlog.txt
+brew update ~/Desktop/brewlog.txt
+brew upgrade --all >> ~/Desktop/brewlog.txt
+echo"" >> ~/Desktop/brewlog.txt
+echo "------------------------------------------------" >> ~/Desktop/brewlog.txt
