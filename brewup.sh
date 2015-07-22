b0ff59f355deecd6def812ad0320aa8cf90aa0b0
Michael Oelkers <moelkers2010@gmail.com>
2015-07-22 14:02:32 -0500

    Adding redirects within brewup for inclusion of desired formatting features.

--- brewup.sh
+++ brewup.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 echo "------------------------------------------------"
-date 
-echo""
-echo""
-brew update
-brew upgrade --all
-echo""
-echo "------------------------------------------------"
+date >> ~/Desktop >> brewlog.txt 
+echo"" >> ~/Desktop >> brewlog.txt
+echo"" >> ~/Desktop >> brewlog.txt
+brew update >> ~/Desktop >> brewlog.txt
+brew upgrade --all >> ~/Desktop >> brewlog.txt
+echo"" >> ~/Desktop >> brewlog.txt
+echo "------------------------------------------------" >> ~/Desktop >> brewlog.txt
