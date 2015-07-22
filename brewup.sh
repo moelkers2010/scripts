c9a59775032ee39b3c5d1a750f7f3af2b67cfd41
Michael Oelkers <moelkers2010@gmail.com>
2015-07-22 14:34:14 -0500

    added missing redirect to brew update command.

--- brewup.sh
+++ brewup.sh
@@ -3,7 +3,7 @@ echo "------------------------------------------------"
 date >> ~/Desktop/brewlog.txt 
 echo"" >> ~/Desktop/brewlog.txt
 echo"" >> ~/Desktop/brewlog.txt
-brew update ~/Desktop/brewlog.txt
+brew update >> ~/Desktop/brewlog.txt
 brew upgrade --all >> ~/Desktop/brewlog.txt
 echo"" >> ~/Desktop/brewlog.txt
 echo "------------------------------------------------" >> ~/Desktop/brewlog.txt
